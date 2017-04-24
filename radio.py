5d8a6a10aa59e63dfeb4cac5db833d7594883676
Jon <jpmunz@gmail.com>
2017-04-23 21:34:28 -0400

    add radio

---
+++ radio.py
@@ -0,0 +1,23 @@
+from GPIOLibrary import GPIOProcessor
+import time
+
+GP = GPIOProcessor()
+
+try:
+
+    receiverPin = GP.getPin33()
+    senderPin = GP.getPin34()
+
+    receiverPin.out()
+    senderPin.input()
+
+
+    for i in range(0,10):
+        Pin34.high()
+        time.sleep(0.5)
+        Pin34.low()
+        time.sleep(0.5)
+
+
+finally:
+    GP.cleanup()
